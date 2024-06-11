787d9f806d8a7048144a63e370d36b43c6eb2ca9
shlok agrawal <shlok.agrawal@hotmail.com>
2024-06-11 16:33:31 +0530

    Added toasts

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ import './App.css'
 import { Dashboard } from './pages/Dashboard'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
  
 
   return (
     <BrowserRouter>
+    <ToastContainer
+position="bottom-right"
+autoClose={3000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="dark"
+
+/>
       <Routes>
+        
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
