5b99b189f48c6c2f9f516e2947c12a80722aa593
shlok agrawal <shlok.agrawal@hotmail.com>
2024-06-16 16:14:18 +0530

    Avatar shows initial of name

--- src/components/ContactName.jsx
+++ src/components/ContactName.jsx
@@ -5,7 +5,7 @@ export function ContactName ({name, id, setSelectedid, setMode}){
     return (<>
         <div className="flex justify-start rounded-full hover:bg-blue-600 p-2 m-5" onClick = {() => {setSelectedid(id); setMode('view')} }>
             <div>
-              <Avatar/>            
+              <Avatar name = {name}/>            
             </div>
             <div className="ml-10 mt-5">
                 {name} 
